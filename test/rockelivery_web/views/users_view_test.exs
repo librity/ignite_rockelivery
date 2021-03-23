4d3c94ae7f9aed7efd4c3dc0977c49c0d48cb2b6
Luis Geniole <luis.geniole@gmail.com>
2021-03-23 20:48:07 -0300

    Write users view tests.

---
+++ test/rockelivery_web/views/users_view_test.exs
@@ -0,0 +1,53 @@
+defmodule RockeliveryWeb.UsersViewTest do
+  use RockeliveryWeb.ConnCase, async: true
+
+  import Phoenix.View
+  import Rockelivery.Factory
+
+  alias RockeliveryWeb.UsersView
+
+  test "renders create.json" do
+    user = build(:user)
+
+    return = render(UsersView, "create.json", user: user)
+
+    assert %{
+             message: "User created successfully",
+             user: %Rockelivery.User{
+               address: "Paper street 123",
+               age: 40,
+               cep: "12345678",
+               cpf: "12345678910",
+               email: "tyler@under.ground",
+               id: "e173f324-9d91-43a9-a45a-e6647abf721c",
+               inserted_at: nil,
+               name: "Tyler Durden",
+               password: "first_rule",
+               password_hash: nil,
+               updated_at: nil
+             }
+           } = return
+  end
+
+  test "renders user.json" do
+    user = build(:user)
+
+    return = render(UsersView, "user.json", user: user)
+
+    assert %{
+             user: %Rockelivery.User{
+               address: "Paper street 123",
+               age: 40,
+               cep: "12345678",
+               cpf: "12345678910",
+               email: "tyler@under.ground",
+               id: "e173f324-9d91-43a9-a45a-e6647abf721c",
+               inserted_at: nil,
+               name: "Tyler Durden",
+               password: "first_rule",
+               password_hash: nil,
+               updated_at: nil
+             }
+           } = return
+  end
+end
