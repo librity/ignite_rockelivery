b20936256e08fbdf0da83d431ba7a1899a1cf1eb
Luis Geniole <luis.geniole@gmail.com>
2021-04-07 20:47:59 -0300

    Add report runner to app supervisor.

--- lib/rockelivery/orders/report_runner.ex
+++ lib/rockelivery/orders/report_runner.ex
@@ -5,10 +5,13 @@ defmodule Rockelivery.Orders.ReportRunner do
 
   alias Rockelivery.Orders.Report
 
-  def start_link, do: GenServer.start_link(__MODULE__, %{})
+  @delay 1000 * 60 * 60 * 24
+
+  def start_link(initial_state \\ %{}), do: GenServer.start_link(__MODULE__, initial_state)
 
   @impl true
   def init(state) do
+    Logger.info("Orders report runner initialized!")
     schedule_report()
 
     {:ok, state}
@@ -16,10 +19,9 @@ defmodule Rockelivery.Orders.ReportRunner do
 
   @impl true
   def handle_info(:generate, state) do
-    Logger.info("Generating report...")
+    Logger.info("Generating orders report...")
 
-    DateTime.utc_now()
-    |> to_string()
+    generate_filename()
     |> Report.create()
 
     schedule_report()
@@ -28,8 +30,14 @@ defmodule Rockelivery.Orders.ReportRunner do
   end
 
   defp schedule_report do
-    # delay = 1000 * 60 * 60
-    delay = 1000 * 60
-    Process.send_after(self(), :generate, delay)
+    Process.send_after(self(), :generate, @delay)
+  end
+
+  defp generate_filename do
+    date =
+      NaiveDateTime.local_now()
+      |> NaiveDateTime.to_iso8601()
+
+    "orders_report_#{date}"
   end
 end
